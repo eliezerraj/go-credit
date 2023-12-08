533949c8964d9a4f58c5a51b051d7b418ee7a71b
eliezerraj <eliezerraj@gmail.com>
2023-12-08 16:05:58 -0300

    1st commit

---
+++ internal/repository/postgre/repositoryCredit.go
@@ -0,0 +1,97 @@
+package postgre
+
+import (
+	"context"
+	"time"
+	"errors"
+
+	_ "github.com/lib/pq"
+	"database/sql"
+
+	"github.com/go-credit/internal/core"
+	"github.com/aws/aws-xray-sdk-go/xray"
+
+)
+
+func (w WorkerRepository) Add(ctx context.Context, tx *sql.Tx ,credit core.AccountStatement) (*core.AccountStatement, error){
+	childLogger.Debug().Msg("Add")
+
+	_, root := xray.BeginSubsegment(ctx, "SQL.Add-Credit")
+	defer func() {
+		root.Close(nil)
+	}()
+
+	stmt, err := tx.Prepare(`INSERT INTO account_statement ( 	fk_account_id, 
+																type_charge,
+																charged_at, 
+																currency,
+																amount,
+																tenant_id) 
+									VALUES($1, $2, $3, $4, $5, $6) `)
+	if err != nil {
+		childLogger.Error().Err(err).Msg("INSERT statement")
+		return nil, errors.New(err.Error())
+	}
+
+	_, err = stmt.ExecContext(	ctx,
+								credit.FkAccountID, 
+								credit.Type,
+								time.Now(),
+								credit.Currency,
+								credit.Amount,
+								credit.TenantID)
+	if err != nil {
+		childLogger.Error().Err(err).Msg("Exec statement")
+		return nil, errors.New(err.Error())
+	}
+
+	defer stmt.Close()
+	return &credit , nil
+}
+
+func (w WorkerRepository) List(ctx context.Context, credit core.AccountStatement) (*[]core.AccountStatement, error){
+	childLogger.Debug().Msg("List")
+
+	_, root := xray.BeginSubsegment(ctx, "SQL.List-Credit")
+	defer func() {
+		root.Close(nil)
+	}()
+
+	client:= w.databaseHelper.GetConnection()
+	
+	result_query := core.AccountStatement{}
+	balance_list := []core.AccountStatement{}
+
+	rows, err := client.QueryContext(ctx, `SELECT 	id, 
+													fk_account_id, 
+													type_charge,
+													charged_at,
+													currency, 
+													amount,																										
+													tenant_id	
+											FROM account_statement 
+											WHERE fk_account_id =$1 order by charged_at desc`, credit.FkAccountID)
+	if err != nil {
+		childLogger.Error().Err(err).Msg("SELECT statement")
+		return nil, errors.New(err.Error())
+	}
+
+	for rows.Next() {
+		err := rows.Scan( 	&result_query.ID, 
+							&result_query.FkAccountID, 
+							&result_query.Type, 
+							&result_query.ChargeAt,
+							&result_query.Currency,
+							&result_query.Amount,
+							&result_query.TenantID,
+						)
+		if err != nil {
+			childLogger.Error().Err(err).Msg("Scan statement")
+			return nil, errors.New(err.Error())
+        }
+		balance_list = append(balance_list, result_query)
+	}
+
+	defer rows.Close()
+	return &balance_list , nil
+}
---
+++ internal/service/credit.go
@@ -0,0 +1,125 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"github.com/rs/zerolog/log"
+
+	"github.com/mitchellh/mapstructure"
+	"github.com/go-credit/internal/core"
+	"github.com/go-credit/internal/erro"
+	"github.com/go-credit/internal/adapter/restapi"
+	"github.com/go-credit/internal/repository/postgre"
+	"github.com/aws/aws-xray-sdk-go/xray"
+
+)
+
+var childLogger = log.With().Str("service", "service").Logger()
+
+type WorkerService struct {
+	workerRepository 		*postgre.WorkerRepository
+	restapi					*restapi.RestApiSConfig
+}
+
+func NewWorkerService(	workerRepository 	*postgre.WorkerRepository,
+						restapi				*restapi.RestApiSConfig) *WorkerService{
+	childLogger.Debug().Msg("NewWorkerService")
+
+	return &WorkerService{
+		workerRepository:	workerRepository,
+		restapi:			restapi,
+	}
+}
+
+func (s WorkerService) SetSessionVariable(ctx context.Context, userCredential string) (bool, error){
+	childLogger.Debug().Msg("SetSessionVariable")
+
+	res, err := s.workerRepository.SetSessionVariable(ctx, userCredential)
+	if err != nil {
+		return false, err
+	}
+
+	return res, nil
+}
+
+func (s WorkerService) Add(ctx context.Context, credit core.AccountStatement) (*core.AccountStatement, error){
+	childLogger.Debug().Msg("Add")
+
+	_, root := xray.BeginSubsegment(ctx, "Service.Add")
+
+	tx, err := s.workerRepository.StartTx(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+		} else {
+			tx.Commit()
+		}
+		root.Close(nil)
+	}()
+
+	childLogger.Debug().Interface("credit:",credit).Msg("")
+
+	credit.Type = "CREDIT"
+	if credit.Amount < 0 {
+		return nil, erro.ErrInvalidAmount
+	}
+
+	rest_interface_data, err := s.restapi.GetData(ctx, credit.AccountID, "/get")
+	if err != nil {
+		return nil, err
+	}
+
+	var account_parsed core.Account
+	err = mapstructure.Decode(rest_interface_data, &account_parsed)
+    if err != nil {
+		childLogger.Error().Err(err).Msg("error parse interface")
+		return nil, errors.New(err.Error())
+    }
+
+	credit.FkAccountID = account_parsed.ID
+	res, err := s.workerRepository.Add(ctx, tx, credit)
+	if err != nil {
+		return nil, err
+	}
+
+	childLogger.Debug().Interface("credit:",credit).Msg("")
+
+	_, err = s.restapi.PostData(ctx, "/add/fund", credit)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (s WorkerService) List(ctx context.Context, credit core.AccountStatement) (*[]core.AccountStatement, error){
+	childLogger.Debug().Msg("List")
+	childLogger.Debug().Interface("credit:",credit).Msg("")
+	
+	_, root := xray.BeginSubsegment(ctx, "Service.List")
+	defer root.Close(nil)
+
+	rest_interface_data, err := s.restapi.GetData(ctx, credit.AccountID, "/get")
+	if err != nil {
+		return nil, err
+	}
+
+	var account_parsed core.Account
+	err = mapstructure.Decode(rest_interface_data, &account_parsed)
+    if err != nil {
+		childLogger.Error().Err(err).Msg("error parse interface")
+		return nil, errors.New(err.Error())
+    }
+
+	credit.FkAccountID = account_parsed.ID
+	res, err := s.workerRepository.List(ctx, credit)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
